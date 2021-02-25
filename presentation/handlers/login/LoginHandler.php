d02947400a9498f954c70f98debe59858ff74709
Matt Sievers <msievers@thegeneral.com>
2021-02-25 08:10:10 -0600

    updates for week 5 CLC work. Updates to all services Movie now has CRUD operations.

--- presentation/handlers/login/LoginHandler.php
+++ presentation/handlers/login/LoginHandler.php
@@ -10,7 +10,8 @@ $userService = new UserBusinessService();
 if($authService->authUser($username, $password)){
     $user = $userService->findUserByUsername($username);
     $_SESSION['id']=$user->getId();
-    header("Location:index.php");
+    $_SESSION['username']=$user->getUsername();
+    header("Location:../../../index.php");
 }else{
     echo "There was an error logging in, please make sure your username and password are correct"."<br>";
 }
--- presentation/handlers/login/LogoutHandler.php
+++ presentation/handlers/login/LogoutHandler.php
@@ -2,9 +2,5 @@
 include_once "../../../header.php";
 require_once "../../../Autoloader.php";
 
-if(isset($_SESSION['id'])){
     session_destroy();
-    header("location:index.php");
-}else{
-    echo"Nothing to log out, please login!"."<br>";
-}
+    header("location:../../../index.php");
